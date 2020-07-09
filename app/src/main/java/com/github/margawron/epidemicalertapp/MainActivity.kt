43d5a6ba482dbae9f171ba0339e8da9752c57cbc
margawron <max2.marcel@gmail.com>
2020-07-09 19:19:09 +0200

    Initial Android app.

---
+++ app/src/main/java/com/github/margawron/epidemicalertapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.margawron.epidemicalertapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
