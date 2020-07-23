ac353b4bda87d13c4824f5a251d43e4bb9510f5c
margawron <max2.marcel@gmail.com>
2020-07-24 01:21:58 +0200

    Create main view with marker on Google Map.

---
+++ app/src/main/java/com/github/margawron/epidemicalertapp/EpidemicAlertApp.kt
@@ -0,0 +1,9 @@
+package com.github.margawron.epidemicalertapp
+
+import android.app.Application
+
+class EpidemicAlertApp : Application() {
+    companion object {
+        val TAG = "EpidemicAlert"
+    }
+}
--- app/src/main/java/com/github/margawron/epidemicalertapp/MainActivity.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/MainActivity.kt
@@ -1,11 +1,113 @@
 package com.github.margawron.epidemicalertapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.github.margawron.epidemicalertapp.util.PermissionSafeguard
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var googleMap: GoogleMap? = null
+
+    private val permissionSafeguard = PermissionSafeguard(
+        this,
+        arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        ),
+        50
+    )
+
+    private val locationRequest = LocationRequest().apply {
+        this.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        this.interval = 1000
+        this.fastestInterval = 500
+    }
+
+    private var marker: Marker? = null
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            locationResult ?: return
+            for (location in locationResult.locations) {
+
+                Log.d(EpidemicAlertApp.TAG, " measurement error " + location.accuracy )
+                val position = LatLng(location.latitude, location.longitude)
+                Log.d(EpidemicAlertApp.TAG, " position " + position )
+
+                marker?.remove()
+                marker = googleMap?.addMarker(MarkerOptions().apply {
+                    position(position)
+                    title(getString(R.string.HERE_YOU_ARE))
+                })
+                googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(position,18F))
+                googleMap?.animateCamera(CameraUpdateFactory.zoomIn())
+                googleMap?.animateCamera(CameraUpdateFactory.zoomTo(18F), 2000, null)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        startLocationUpdates()
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.onCreate(savedInstanceState)
+        mapFragment.getMapAsync(this)
+
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    )
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when {
+            requestCode == 50 -> {
+                startLocationUpdates()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun startLocationUpdates() {
+        permissionSafeguard.doIfPossibleOrRequestPermission {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onMapReady(incomingGoogleMap: GoogleMap?) {
+        googleMap = incomingGoogleMap
+    }
+
+
 }
