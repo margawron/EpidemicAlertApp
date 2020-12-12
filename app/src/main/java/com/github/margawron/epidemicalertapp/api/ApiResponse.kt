86e3f31b99c17cf1c79b992872c3259ab86fce58
margawron <max2.marcel@gmail.com>
2020-12-12 23:54:29 +0100

    Make errorField nullable and ignore code field.

--- app/src/main/java/com/github/margawron/epidemicalertapp/api/ApiResponse.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/api/ApiResponse.kt
@@ -1,8 +1,10 @@
 package com.github.margawron.epidemicalertapp.api
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 sealed class ApiResponse<out T : Any> {
     data class SuccessWithBody<T : Any>(val body: T) : ApiResponse<T>()
     data class SuccessWithoutBody(val code: Int, val message: String) : ApiResponse<Nothing>()
-    data class Error(val errorName: String, val errorField: String, val errorMessage: String, var code: Int? = null) :
+    data class Error(val errorName: String, val errorField: String?, val errorMessage: String, @JsonIgnore var code: Int? = null) :
         ApiResponse<Nothing>()
 }
