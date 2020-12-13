808ccaa16d8d9d9b2e82f3c1c1fcbe3c9d999c55
margawron <max2.marcel@gmail.com>
2020-12-13 15:39:22 +0100

    Create main application activity.

---
+++ app/src/main/java/com/github/margawron/epidemicalertapp/LocationDisplayActivity.kt
@@ -0,0 +1,13 @@
+package com.github.margawron.epidemicalertapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LocationDisplayActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.location_display_activity)
+    }
+}
