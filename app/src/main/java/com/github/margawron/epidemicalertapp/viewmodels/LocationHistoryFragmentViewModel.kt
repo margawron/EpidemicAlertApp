46dec24d25aba013d3144f0d32559f1e2bc03d5b
margawron <max2.marcel@gmail.com>
2020-12-16 00:13:47 +0100

    Delete LocationHistoryFragmentViewModel.kt.

--- app/src/main/java/com/github/margawron/epidemicalertapp/viewmodels/LocationHistoryFragmentViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.github.margawron.epidemicalertapp.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class LocationHistoryFragmentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
