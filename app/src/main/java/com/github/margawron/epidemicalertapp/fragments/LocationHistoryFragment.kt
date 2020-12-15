f3033ddab84d0ccf066171bd5d2a63bb6e0eb73a
margawron <max2.marcel@gmail.com>
2020-12-15 01:35:26 +0100

    Load map for location_history_fragment.xml

--- app/src/main/java/com/github/margawron/epidemicalertapp/fragments/LocationHistoryFragment.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/fragments/LocationHistoryFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.github.margawron.epidemicalertapp.viewmodels.LocationHistoryFragmentViewModel
 import com.github.margawron.epidemicalertapp.R
+import com.github.margawron.epidemicalertapp.databinding.LocationHistoryFragmentBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 
 class LocationHistoryFragment : Fragment() {
 
@@ -16,18 +20,28 @@ class LocationHistoryFragment : Fragment() {
     }
 
     private lateinit var viewModel: LocationHistoryFragmentViewModel
+    private lateinit var binding: LocationHistoryFragmentBinding
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.location_history_fragment, container, false)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.location_history_fragment, container, false)
+        viewModel = ViewModelProvider(this).get(LocationHistoryFragmentViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LocationHistoryFragmentViewModel::class.java)
-        // TODO: Use the ViewModel
+        val fragment = childFragmentManager.findFragmentById(R.id.mapFragment)
+        val mapFragment = fragment as SupportMapFragment
+        mapFragment.onCreate(savedInstanceState)
+        mapFragment.getMapAsync {
+            googleMap = it
+        }
     }
-
 }
