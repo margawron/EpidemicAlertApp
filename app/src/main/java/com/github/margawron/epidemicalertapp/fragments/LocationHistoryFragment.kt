b460bf4b986e8ac313ad3464a4b1356d4b1d8b4c
margawron <max2.marcel@gmail.com>
2020-12-18 21:01:30 +0100

    Move implementation details from LocationHistoryFragment.kt to LocationHistoryViewModel.kt

--- app/src/main/java/com/github/margawron/epidemicalertapp/fragments/LocationHistoryFragment.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/fragments/LocationHistoryFragment.kt
@@ -1,27 +1,18 @@
 package com.github.margawron.epidemicalertapp.fragments
 
-import android.app.DatePickerDialog
-import android.graphics.Color
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
+import androidx.fragment.app.viewModels
 import com.github.margawron.epidemicalertapp.R
-import com.github.margawron.epidemicalertapp.auth.AuthManager
-import com.github.margawron.epidemicalertapp.data.measurments.Measurement
-import com.github.margawron.epidemicalertapp.data.measurments.MeasurementRepository
 import com.github.margawron.epidemicalertapp.databinding.LocationHistoryFragmentBinding
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
+import com.github.margawron.epidemicalertapp.viewmodels.LocationHistoryFragmentViewModel
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
 import dagger.hilt.android.AndroidEntryPoint
-import java.time.LocalDate
-import java.time.ZoneId
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class LocationHistoryFragment : Fragment() {
@@ -30,17 +21,7 @@ class LocationHistoryFragment : Fragment() {
         fun newInstance() = LocationHistoryFragment()
     }
 
-    private lateinit var googleMap: GoogleMap
-    private var polyline: Polyline? = null
-    private var isMapTouchedByUser: Boolean = false
-    private var displayedDate = LocalDate.now()
-    private lateinit var selectedDateUserLocation: LiveData<List<Measurement>>
-
-    @Inject
-    lateinit var measurementRepository: MeasurementRepository
-
-    @Inject
-    lateinit var authManager: AuthManager
+    private val viewModel by viewModels<LocationHistoryFragmentViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,97 +30,14 @@ class LocationHistoryFragment : Fragment() {
         val binding: LocationHistoryFragmentBinding =
             DataBindingUtil.inflate(inflater, R.layout.location_history_fragment, container, false)
         binding.lifecycleOwner = this
+        binding.vm = viewModel
 
-        setupMap(savedInstanceState, binding)
-
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        isMapTouchedByUser = false
-    }
-
-    private fun setupMap(
-        savedInstanceState: Bundle?,
-        binding: LocationHistoryFragmentBinding
-    ) {
         val fragment = childFragmentManager.findFragmentById(R.id.locationHistoryMapFragment)
         val mapFragment = fragment as SupportMapFragment
-        mapFragment.onCreate(savedInstanceState)
         mapFragment.getMapAsync {
-            googleMap = it
-            setupHistoryLineGenerator()
+            viewModel.googleMap = it
+            viewModel.refresh()
         }
-        binding.root.setOnClickListener { isMapTouchedByUser = true }
-        binding.locationFragmentFab.setOnClickListener {
-            val listener =
-                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-                    displayedDate = LocalDate.of(year, month + 1, dayOfMonth)
-                    setupHistoryLineGenerator()
-                }
-            val pickerDialog = DatePickerDialog(
-                requireContext(),
-                listener,
-                displayedDate.year,
-                displayedDate.monthValue - 1,
-                displayedDate.dayOfMonth
-            )
-            pickerDialog.show()
-        }
-    }
-
-    private fun setupHistoryLineGenerator() {
-        selectedDateUserLocation = measurementRepository.getLocationsForDay(
-            authManager.getLoggedInUser()!!,
-            displayedDate
-        )
-        selectedDateUserLocation.observe(viewLifecycleOwner) { list ->
-            val polyLineOptions = with(PolylineOptions()) {
-                width(3.0f)
-                color(Color.RED)
-                geodesic(true)
-            }
-            googleMap.clear()
-            setupMap(list, polyLineOptions)
-        }
-    }
-
-    private fun setupMap(
-        list: List<Measurement>,
-        polyLineOptions: PolylineOptions
-    ) {
-        if (list.isNotEmpty()) {
-            val mapBoundsBuilder = LatLngBounds.Builder()
-            list.forEach {
-                val latLng = LatLng(it.latitude, it.longitude)
-
-                polyLineOptions.add(latLng)
-
-                mapBoundsBuilder.include(latLng)
-
-                val markerOptions = MarkerOptions()
-                markerOptions.position(latLng)
-                markerOptions.title(
-                    it.measurementTime.atZone(ZoneId.systemDefault()).toString()
-                )
-
-                googleMap.addMarker(markerOptions)
-            }
-            val latLngBounds = LatLngBounds.builder()
-            list.takeLast(10).forEach {
-                latLngBounds.include(LatLng(it.latitude,it.longitude))
-            }
-            polyline = googleMap.addPolyline(polyLineOptions)
-            val latestLL = list.last()
-            if (!isMapTouchedByUser) {
-                googleMap.animateCamera(
-                    CameraUpdateFactory.newLatLngZoom(
-                        LatLng(latestLL.latitude, latestLL.longitude),
-                        18.0f
-                    )
-                )
-            }
-        }
+        return binding.root
     }
 }
---
+++ app/src/main/java/com/github/margawron/epidemicalertapp/viewmodels/LocationHistoryFragmentViewModel.kt
@@ -0,0 +1,113 @@
+package com.github.margawron.epidemicalertapp.viewmodels
+
+import android.app.DatePickerDialog
+import android.content.Context
+import android.graphics.Color
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.margawron.epidemicalertapp.auth.AuthManager
+import com.github.margawron.epidemicalertapp.data.measurments.Measurement
+import com.github.margawron.epidemicalertapp.data.measurments.MeasurementRepository
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import dagger.hilt.android.qualifiers.ActivityContext
+import java.time.LocalDate
+import java.time.ZoneId
+
+class LocationHistoryFragmentViewModel @ViewModelInject internal constructor(
+    private val authManager: AuthManager,
+    private val measurementRepository: MeasurementRepository,
+    @ActivityContext private val context: Context
+) : ViewModel() {
+
+    lateinit var googleMap: GoogleMap
+
+    private var displayedDate = LocalDate.now()
+    private lateinit var selectedDateUserLocation: LiveData<List<Measurement>>
+    var isMapTouchedByUser = false
+
+    fun refresh(){
+        setupHistoryLineGenerator()
+    }
+
+    fun changeDisplayedDate() {
+        val listener =
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                displayedDate = LocalDate.of(year, month + 1, dayOfMonth)
+                setupHistoryLineGenerator()
+            }
+        val pickerDialog = DatePickerDialog(
+            context,
+            listener,
+            displayedDate.year,
+            displayedDate.monthValue - 1,
+            displayedDate.dayOfMonth
+        )
+        pickerDialog.show()
+    }
+
+    fun mapTouchedByUser(){
+        isMapTouchedByUser = true
+    }
+
+    private fun setupHistoryLineGenerator() {
+        selectedDateUserLocation = measurementRepository.getLocationsForDay(
+            authManager.getLoggedInUser(),
+            displayedDate
+        )
+        selectedDateUserLocation.observeForever { list ->
+            val polyLineOptions = with(PolylineOptions()) {
+                width(3.0f)
+                color(Color.RED)
+                geodesic(true)
+            }
+            googleMap.clear()
+            setupMap(list, polyLineOptions)
+        }
+    }
+
+
+    private fun setupMap(
+        list: List<Measurement>,
+        polyLineOptions: PolylineOptions
+    ) {
+        if (list.isNotEmpty()) {
+            val mapBoundsBuilder = LatLngBounds.Builder()
+            list.forEach {
+                val latLng = LatLng(it.latitude, it.longitude)
+
+                polyLineOptions.add(latLng)
+
+                mapBoundsBuilder.include(latLng)
+
+                val markerOptions = MarkerOptions()
+                markerOptions.position(latLng)
+                markerOptions.title(
+                    it.measurementTime.atZone(ZoneId.systemDefault()).toString()
+                )
+
+                googleMap.addMarker(markerOptions)
+            }
+            val latLngBounds = LatLngBounds.builder()
+            list.takeLast(10).forEach {
+                latLngBounds.include(LatLng(it.latitude, it.longitude))
+            }
+            googleMap.addPolyline(polyLineOptions)
+            val latestLL = list.last()
+            if (!isMapTouchedByUser) {
+                googleMap.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(latestLL.latitude, latestLL.longitude),
+                        18.0f
+                    )
+                )
+            }
+        }
+    }
+}
