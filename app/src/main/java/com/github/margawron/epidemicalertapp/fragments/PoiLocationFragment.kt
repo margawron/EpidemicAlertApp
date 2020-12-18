9f10347e9351c8f4c5570aefda356f6178f7ba1d
margawron <max2.marcel@gmail.com>
2020-12-18 21:22:46 +0100

    Implement basic PoiLocationFragment.kt

--- app/src/main/java/com/github/margawron/epidemicalertapp/fragments/PoiLocationFragment.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/fragments/PoiLocationFragment.kt
@@ -1,33 +1,42 @@
 package com.github.margawron.epidemicalertapp.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.github.margawron.epidemicalertapp.viewmodels.PoiLocationFragmentViewModel
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.github.margawron.epidemicalertapp.R
+import com.github.margawron.epidemicalertapp.databinding.PoiLocationFragmentBinding
+import com.github.margawron.epidemicalertapp.viewmodels.PoiLocationFragmentViewModel
+import com.google.android.gms.maps.SupportMapFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PoiLocationFragment : Fragment() {
 
     companion object {
         fun newInstance() = PoiLocationFragment()
     }
 
-    private lateinit var viewModel: PoiLocationFragmentViewModel
+    private val viewModel by viewModels<PoiLocationFragmentViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.poi_location_fragment, container, false)
-    }
+    ): View {
+        val binding: PoiLocationFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.poi_location_fragment, container, false)
+        binding.lifecycleOwner = this
+        val fragment = childFragmentManager.findFragmentById(R.id.locationPoiMapFragment)
+        val mapFragment = fragment as SupportMapFragment
+        mapFragment.onCreate(savedInstanceState)
+        mapFragment.getMapAsync {
+            viewModel.googleMap = it
+            viewModel.refresh()
+        }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PoiLocationFragmentViewModel::class.java)
-        // TODO: Use the ViewModel
+        return binding.root
     }
-
 }
--- app/src/main/java/com/github/margawron/epidemicalertapp/viewmodels/PoiLocationFragmentViewModel.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/viewmodels/PoiLocationFragmentViewModel.kt
@@ -1,7 +1,33 @@
 package com.github.margawron.epidemicalertapp.viewmodels
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.github.margawron.epidemicalertapp.data.measurments.MeasurementRepository
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 
-class PoiLocationFragmentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class PoiLocationFragmentViewModel @ViewModelInject internal constructor(
+    private val measurementRepository: MeasurementRepository
+)
+    : ViewModel() {
+
+    private val currentLocation = measurementRepository.getCurrentLocation()
+    lateinit var googleMap: GoogleMap
+
+    fun refresh(){
+        showPointsOfInterests()
+    }
+
+    private fun showPointsOfInterests(){
+        currentLocation.observeForever { location ->
+            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    location.latitude,
+                    location.longitude
+                ), 12.0f
+            )
+            googleMap.animateCamera(cameraUpdate)
+        }
+    }
 }
