e6818190fabe9b0439c0a3166b5594149138d281
margawron <max2.marcel@gmail.com>
2020-12-28 21:35:35 +0100

    Prepare ZoneFragmentViewModel to implement zones.

--- app/src/main/java/com/github/margawron/epidemicalertapp/viewmodels/ZoneFragmentViewModel.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/viewmodels/ZoneFragmentViewModel.kt
@@ -2,6 +2,7 @@ package com.github.margawron.epidemicalertapp.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.github.margawron.epidemicalertapp.data.measurments.Measurement
@@ -27,16 +28,25 @@ class ZoneFragmentViewModel @ViewModelInject internal constructor(
         val currentLocation = measurementRepository.getLastLocation()
         val observer = Observer<Measurement?>{ measurement ->
             measurement?.apply {
-                val cameraUpdate = CameraUpdateFactory.newLatLngZoom(
-                    LatLng(
-                        measurement.latitude,
-                        measurement.longitude
-                    ), 10.0f
-                )
-                googleMap.animateCamera(cameraUpdate)
-                currentLocation.removeObservers(lifecycleOwner)
+                showZone(measurement, currentLocation)
             }
         }
         currentLocation.observe(lifecycleOwner, observer)
+    }
+
+    private fun showZone(
+        measurement: Measurement,
+        currentLocation: LiveData<Measurement?>
+    ) {
+        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+            LatLng(
+                measurement.latitude,
+                measurement.longitude
+            ), 10.0f
+        )
+        googleMap.animateCamera(cameraUpdate)
+        currentLocation.removeObservers(lifecycleOwner)
+        // TODO geocode current location
+        // Get data from server
     }
 }
