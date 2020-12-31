f1923f2004c6fcf47e17df06ff1c5a67d6bd560e
margawron <max2.marcel@gmail.com>
2020-12-31 19:25:22 +0100

    Add endpoint for fetching users by name.

--- app/src/main/java/com/github/margawron/epidemicalertapp/api/users/UserService.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/api/users/UserService.kt
@@ -2,8 +2,12 @@ package com.github.margawron.epidemicalertapp.api.users
 
 import com.github.margawron.epidemicalertapp.api.common.ApiResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface UserService {
     @GET("users/self")
     suspend fun getSelf(): ApiResponse<UserDto>
+
+    @GET("users/name/{name}")
+    suspend fun getUserWithNameLike(@Path("name") nameLike: String): ApiResponse<List<UserDto>>
 }
