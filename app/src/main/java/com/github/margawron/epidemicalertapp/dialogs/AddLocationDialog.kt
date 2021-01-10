6a18726f57f2ce77d5c992cacd523fc55c2be585
margawron <max2.marcel@gmail.com>
2021-01-10 15:27:44 +0100

    Rename add location dialog.

--- app/src/main/java/com/github/margawron/epidemicalertapp/dialogs/AddLocationDialog.kt
+++ app/src/main/java/com/github/margawron/epidemicalertapp/dialogs/AddLocationDialog.kt
@@ -6,7 +6,7 @@ import android.view.Window
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.databinding.DataBindingUtil
 import com.github.margawron.epidemicalertapp.R
-import com.github.margawron.epidemicalertapp.databinding.AddLocationAlertBinding
+import com.github.margawron.epidemicalertapp.databinding.AddLocationDialogBinding
 import com.github.margawron.epidemicalertapp.databinds.viewmodels.dialogs.AddLocationViewModel
 
 class AddLocationDialog internal constructor(
@@ -17,13 +17,13 @@ class AddLocationDialog internal constructor(
         AddLocationViewModel(onAddLocationResult)
     }
 
-    lateinit var binding: AddLocationAlertBinding
+    lateinit var binding: AddLocationDialogBinding
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        val binding: AddLocationAlertBinding =
-            DataBindingUtil.inflate(layoutInflater, R.layout.add_location_alert, null, false)
+        val binding: AddLocationDialogBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.add_location_dialog, null, false)
         binding.lifecycleOwner = activity
         binding.vm = viewModel
         viewModel.dialog = this
